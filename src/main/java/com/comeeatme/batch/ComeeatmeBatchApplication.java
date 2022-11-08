90625393e3df5ea611a91d51e197f980a04ac260
Cho-D-YoungRae <yrc9229@gmail.com>
2022-11-08 17:29:38 +0900

    init project

---
+++ src/main/java/com/comeeatme/batch/ComeeatmeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.comeeatme.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeeatmeBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeeatmeBatchApplication.class, args);
+    }
+
+}
