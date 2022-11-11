f07528cf7923c2eae85159ed11f7cc7f69e10add
Cho-D-YoungRae <yrc9229@gmail.com>
2022-11-11 14:40:53 +0900

    chore: db schema sql 생성

---
+++ src/main/resources/schema-mysql.sql
@@ -0,0 +1,200 @@
+drop table if exists restaurant;
+
+drop table if exists incomplete_restaurant;
+
+drop table if exists open_info;
+
+-- Restaurant
+create table restaurant
+(
+    restaurant_id     bigint       not null auto_increment,
+    name              varchar(45)  not null,
+    phone             varchar(25)  not null,
+    address_name      varchar(255) not null,
+    road_address_name varchar(255) not null,
+    location          point        not null srid 4326,
+    use_yn            bit          not null,
+    created_at        datetime(6)  not null,
+    last_modified_at  datetime(6)  not null,
+    primary key (restaurant_id)
+) engine = InnoDB;
+
+create index IX_restaurant_name on restaurant (name);
+
+create spatial index IX_restaurant_location on restaurant (location);
+
+
+-- IncompleteRestaurant
+create table incomplete_restaurant
+(
+    incomplete_restaurant_id   bigint auto_increment,
+    name                       varchar(45),
+    phone                      varchar(25),
+    address_name               varchar(255),
+    road_address_name          varchar(255),
+    open_info_management_num   varchar(65),
+    open_info_service_id       varchar(15),
+    open_info_name             varchar(15),
+    open_info_category         varchar(15),
+    open_info_permission_date  date,
+    open_info_last_modified_at datetime(6),
+    use_yn                     bit         not null,
+    created_at                 datetime(6) not null,
+    last_modified_at           datetime(6) not null,
+    primary key (incomplete_restaurant_id)
+) engine = InnoDB;
+
+create index IX_incomplete_restaurant_name on incomplete_restaurant (name);
+
+create index IX_incomplete_restaurant_open_info_management_num on incomplete_restaurant (open_info_management_num);
+
+
+-- OpenInfo
+create table open_info
+(
+    open_info_id     bigint      not null auto_increment,
+    restaurant_id    bigint      not null,
+    management_num   varchar(65) not null,
+    service_id       varchar(15) not null,
+    name             varchar(15) not null,
+    category         varchar(15) not null,
+    permission_date  date        not null,
+    last_modified_at datetime(6) not null,
+    primary key (open_info_id)
+) engine = InnoDB;
+
+alter table open_info
+    add constraint UK_open_info_restaurant unique (restaurant_id);
+
+alter table open_info
+    add constraint UK_open_info_management_num unique (management_num);
+
+-- Batch
+DROP TABLE IF EXISTS BATCH_STEP_EXECUTION_CONTEXT;
+DROP TABLE IF EXISTS BATCH_JOB_EXECUTION_CONTEXT;
+DROP TABLE IF EXISTS BATCH_STEP_EXECUTION;
+DROP TABLE IF EXISTS BATCH_JOB_EXECUTION_PARAMS;
+DROP TABLE IF EXISTS BATCH_JOB_EXECUTION;
+DROP TABLE IF EXISTS BATCH_JOB_INSTANCE;
+
+DROP TABLE IF EXISTS BATCH_STEP_EXECUTION_SEQ;
+DROP TABLE IF EXISTS BATCH_JOB_EXECUTION_SEQ;
+DROP TABLE IF EXISTS BATCH_JOB_SEQ;
+
+CREATE TABLE BATCH_JOB_INSTANCE
+(
+    JOB_INSTANCE_ID BIGINT       NOT NULL PRIMARY KEY,
+    VERSION         BIGINT,
+    JOB_NAME        VARCHAR(100) NOT NULL,
+    JOB_KEY         VARCHAR(32)  NOT NULL,
+    constraint JOB_INST_UN unique (JOB_NAME, JOB_KEY)
+) ENGINE = InnoDB;
+
+CREATE TABLE BATCH_JOB_EXECUTION
+(
+    JOB_EXECUTION_ID           BIGINT        NOT NULL PRIMARY KEY,
+    VERSION                    BIGINT,
+    JOB_INSTANCE_ID            BIGINT        NOT NULL,
+    CREATE_TIME                DATETIME(6)   NOT NULL,
+    START_TIME                 DATETIME(6) DEFAULT NULL,
+    END_TIME                   DATETIME(6) DEFAULT NULL,
+    STATUS                     VARCHAR(10),
+    EXIT_CODE                  VARCHAR(2500),
+    EXIT_MESSAGE               VARCHAR(2500),
+    LAST_UPDATED               DATETIME(6),
+    JOB_CONFIGURATION_LOCATION VARCHAR(2500) NULL,
+    constraint JOB_INST_EXEC_FK foreign key (JOB_INSTANCE_ID)
+        references BATCH_JOB_INSTANCE (JOB_INSTANCE_ID)
+) ENGINE = InnoDB;
+
+CREATE TABLE BATCH_JOB_EXECUTION_PARAMS
+(
+    JOB_EXECUTION_ID BIGINT       NOT NULL,
+    TYPE_CD          VARCHAR(6)   NOT NULL,
+    KEY_NAME         VARCHAR(100) NOT NULL,
+    STRING_VAL       VARCHAR(250),
+    DATE_VAL         DATETIME(6) DEFAULT NULL,
+    LONG_VAL         BIGINT,
+    DOUBLE_VAL       DOUBLE PRECISION,
+    IDENTIFYING      CHAR(1)      NOT NULL,
+    constraint JOB_EXEC_PARAMS_FK foreign key (JOB_EXECUTION_ID)
+        references BATCH_JOB_EXECUTION (JOB_EXECUTION_ID)
+) ENGINE = InnoDB;
+
+CREATE TABLE BATCH_STEP_EXECUTION
+(
+    STEP_EXECUTION_ID  BIGINT       NOT NULL PRIMARY KEY,
+    VERSION            BIGINT       NOT NULL,
+    STEP_NAME          VARCHAR(100) NOT NULL,
+    JOB_EXECUTION_ID   BIGINT       NOT NULL,
+    START_TIME         DATETIME(6)  NOT NULL,
+    END_TIME           DATETIME(6) DEFAULT NULL,
+    STATUS             VARCHAR(10),
+    COMMIT_COUNT       BIGINT,
+    READ_COUNT         BIGINT,
+    FILTER_COUNT       BIGINT,
+    WRITE_COUNT        BIGINT,
+    READ_SKIP_COUNT    BIGINT,
+    WRITE_SKIP_COUNT   BIGINT,
+    PROCESS_SKIP_COUNT BIGINT,
+    ROLLBACK_COUNT     BIGINT,
+    EXIT_CODE          VARCHAR(2500),
+    EXIT_MESSAGE       VARCHAR(2500),
+    LAST_UPDATED       DATETIME(6),
+    constraint JOB_EXEC_STEP_FK foreign key (JOB_EXECUTION_ID)
+        references BATCH_JOB_EXECUTION (JOB_EXECUTION_ID)
+) ENGINE = InnoDB;
+
+CREATE TABLE BATCH_STEP_EXECUTION_CONTEXT
+(
+    STEP_EXECUTION_ID  BIGINT        NOT NULL PRIMARY KEY,
+    SHORT_CONTEXT      VARCHAR(2500) NOT NULL,
+    SERIALIZED_CONTEXT TEXT,
+    constraint STEP_EXEC_CTX_FK foreign key (STEP_EXECUTION_ID)
+        references BATCH_STEP_EXECUTION (STEP_EXECUTION_ID)
+) ENGINE = InnoDB;
+
+CREATE TABLE BATCH_JOB_EXECUTION_CONTEXT
+(
+    JOB_EXECUTION_ID   BIGINT        NOT NULL PRIMARY KEY,
+    SHORT_CONTEXT      VARCHAR(2500) NOT NULL,
+    SERIALIZED_CONTEXT TEXT,
+    constraint JOB_EXEC_CTX_FK foreign key (JOB_EXECUTION_ID)
+        references BATCH_JOB_EXECUTION (JOB_EXECUTION_ID)
+) ENGINE = InnoDB;
+
+CREATE TABLE BATCH_STEP_EXECUTION_SEQ
+(
+    ID         BIGINT  NOT NULL,
+    UNIQUE_KEY CHAR(1) NOT NULL,
+    constraint UNIQUE_KEY_UN unique (UNIQUE_KEY)
+) ENGINE = InnoDB;
+
+INSERT INTO BATCH_STEP_EXECUTION_SEQ (ID, UNIQUE_KEY)
+select *
+from (select 0 as ID, '0' as UNIQUE_KEY) as tmp
+where not exists(select * from BATCH_STEP_EXECUTION_SEQ);
+
+CREATE TABLE BATCH_JOB_EXECUTION_SEQ
+(
+    ID         BIGINT  NOT NULL,
+    UNIQUE_KEY CHAR(1) NOT NULL,
+    constraint UNIQUE_KEY_UN unique (UNIQUE_KEY)
+) ENGINE = InnoDB;
+
+INSERT INTO BATCH_JOB_EXECUTION_SEQ (ID, UNIQUE_KEY)
+select *
+from (select 0 as ID, '0' as UNIQUE_KEY) as tmp
+where not exists(select * from BATCH_JOB_EXECUTION_SEQ);
+
+CREATE TABLE BATCH_JOB_SEQ
+(
+    ID         BIGINT  NOT NULL,
+    UNIQUE_KEY CHAR(1) NOT NULL,
+    constraint UNIQUE_KEY_UN unique (UNIQUE_KEY)
+) ENGINE = InnoDB;
+
+INSERT INTO BATCH_JOB_SEQ (ID, UNIQUE_KEY)
+select *
+from (select 0 as ID, '0' as UNIQUE_KEY) as tmp
+where not exists(select * from BATCH_JOB_SEQ);
