de4488718877c079fb3db8433fac8da483e40617
Cho-D-YoungRae <yrc9229@gmail.com>
2022-11-18 02:42:01 +0900

    prune: InvlalidRepository 도메인 제거

--- src/main/java/com/comeeatme/batch/domain/repository/InvalidRestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.comeeatme.batch.domain.repository;
-
-import com.comeeatme.batch.domain.InvalidRestaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvalidRestaurantRepository extends JpaRepository<InvalidRestaurant, Long> {
-}
